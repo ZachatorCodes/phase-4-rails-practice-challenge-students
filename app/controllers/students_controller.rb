d7b9494041884702e67ca1d70765a2ca8eff5ed1
Zach Schwartz <zachatorcodes@gmail.com>
2023-09-04 14:37:53 -0600

    Created index and show routes and handler methods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,19 @@
 class StudentsController < ApplicationController
+
+  def index
+    students = Student.all
+    render json: students, status: :ok
+  end
+
+  def show
+    student = find_student
+    render json: student, status: :ok
+  end
+
+  private
+
+  def find_student
+    Student.find(params[:id])
+  end
+
 end
